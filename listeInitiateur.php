833cf401423455d484644ada4d147d7ebdcf2229
QuentinLireux <21802144@campus.unicaen.fr>
2019-06-12 14:38:04 +0200

    modification erreur requete MYSQL

--- listeInitiateur.php
+++ listeInitiateur.php
@@ -1,9 +1,9 @@
 <?php
-	$req = $bdd->prepare('SELECT MEM_NUM,MEM_NOM,MEM_PRENOM,MEM_MAIL FROM  ORDER BY desc');
-	$req->execute(array($_GET['MEM_NUM'],$_GET['MEM_NOM'],$_GET['MEM_PRENOM'],$_GET['MEM_MAIL']);
+	$req = $bdd->prepare('SELECT MEM_NUM,MEM_NOM,MEM_PRENOM,MEM_MAIL FROM  ORDER BY MEM_NUM desc');
+	$req->execute(array(,$_GET['MEM_NOM'],$_GET['MEM_PRENOM'],$_GET['MEM_MAIL']);
 	while ($donnees = $req->fetch())
 	{
-		echo $donnees['MEM_NUM'].$donnees['MEM_NOM'].$donnees['MEM_PRENOM'].$donnees['MEM_MAIL'];
+		echo $donnees['MEM_NOM'].$donnees['MEM_PRENOM'].$donnees['MEM_MAIL'];
 	}
 	$req->closeCursor();
 ?>
