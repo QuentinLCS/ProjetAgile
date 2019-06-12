43cac1197e02c5f705942876e65ce0ebf356f00e
Quentin Fontaine <21800691@etu.unicaen.fr>
2019-06-12 15:02:29 +0200

    ajout CreeEleve

---
+++ controller/CreeEleve.php
@@ -0,0 +1,34 @@
+<?php
+//Recuperation des donnees
+$prenom = $_POST['prenom'];
+$nom = $_POST['nom'];
+
+//Connection Base de Donnee
+$dbhost = 'localhost';
+$dbuser = 'agile8';
+$dbpass = 'ahV2FeemahM6Jiex';
+$dsn = 'mysql:host=localhost;dbname=agile8_bd;charset=utf8';
+
+try {
+  $pdoConnection = new PDO($dsn, $dbuser, $dbpass);
+  $pdoConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+  echo "Erreur connection : ".$e->getMessage();
+}
+
+//Insertion des valeurs dans la base de donnee
+$insertsql = <<<HEREDOC
+INSERT INTO PLO_MEMBRE VALUES ('$num', '$prenom', '$nom')
+HEREDOC;
+
+$rs1 = $pdoConnection->prepare($insertsql);
+$rs1->execute();
+
+
+
+echo "test";
+
+
+
+
+ ?>
