928e74d3fdb86667ce4a20542683ff4347fd4e6c
Gérémy L'Ecuyer <chupa4000@gmail.com>
2019-06-12 15:59:15 +0200

    modif init

--- view/frontend/connexionMySQL.php
+++ view/frontend/connexionMySQL.php
@@ -1,10 +1,15 @@
 <?php
-try
-{
-	$bdd = new PDO('mysql:localhost;dbname=agile8_bd;charset=utf8','agile8','ahV2FeemahM6Jiex');
-}
-catch (Exception $e)
-{
-    die('Erreur : ' . $e->getMessage());
+//Connection Base de Donnee
+
+$dbhost = 'localhost';
+$dbuser = 'agile8';
+$dbpass = 'ahV2FeemahM6Jiex';
+$dsn = 'mysql:host=localhost;dbname=agile8_bd;charset=utf8';
+
+try {
+  $pdoConnection = new PDO($dsn, $dbuser, $dbpass);
+  $pdoConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+  echo "Erreur connection : ".$e->getMessage();
 }
 ?>
