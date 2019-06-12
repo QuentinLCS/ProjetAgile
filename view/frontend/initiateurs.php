dc8f466ef7c63d27662e7c5f7344f64d3c3e99f7
Quentin Fontaine <21800691@etu.unicaen.fr>
2019-06-12 16:49:19 +0200

    dec

--- view/frontend/initiateurs.php
+++ view/frontend/initiateurs.php
@@ -14,13 +14,13 @@ try {
 }
 
 $req = <<<HEREDOC
-SELECT MEM_NUM,MEM_NOM,MEM_PRENOM,MEM_MAIL FROM PLO_MEMBRE ORDER BY MEM_NUM desc
+SELECT MEM_ROLE,MEM_NOM,MEM_PRENOM,MEM_MAIL FROM PLO_MEMBRE ORDER BY MEM_NUM desc
 HEREDOC;
 
 $res = $pdoConnection->query($req);
 while ($donnees = $res->fetch())
 {
-    echo htmlspecialchars($donnees['MEM_NUM']).htmlspecialchars($donnees['MEM_NOM']).htmlspecialchars($donnees['MEM_PRENOM']).htmlspecialchars($donnees['MEM_MAIL'])?><br>;
+    echo htmlspecialchars($donnees['MEM_ROLE']) . ' ' .htmlspecialchars($donnees['MEM_NOM']). ' '.htmlspecialchars($donnees['MEM_PRENOM']).' '?><br>;
     <a class="waves-effect waves-light btn" href="#registerInitiateur">Directeur</a>
     <a class="waves-effect waves-light btn" href="#registerInitiateur">Responsable</a>
     <a class="waves-effect waves-light btn" href="#registerInitiateur">Initiateur</a><?php
