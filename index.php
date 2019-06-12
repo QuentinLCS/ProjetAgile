0d9a5817af37cd72dc19b5b1c3731a292759477c
Quentin <21802953@etu.unicaen.fr>
2019-06-12 10:11:24 +0200

    Les bases

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+include_once("controller/Menu.php");
+
+$title = "Plongée | ";
+$pageRepertory = "view/frontend/";
+if (isset($_GET["page"])) {
+    $page = $_GET["page"];
+    if  (!file_exists($pageRepertory.strtolower($page).".php")) $page = "Error_404";
+    $title = $title . $page;
+} else {
+    $page = "home";
+    $title = $title . "Home";
+}
+?>
+
+<html lang="fr">
+    <head>
+        <?php include_once($pageRepertory."head.php"); ?>
+        <title><?php echo $title ?></title>
+    </head>
+
+    <body>
+        <header>
+            <?php
+                include_once($pageRepertory."navbar.php");
+                include_once($pageRepertory."login.php")
+            ?>
+        </header>
+
+        <main>
+            <?php include_once($pageRepertory.strtolower($page).".php"); ?>
+        </main>
+
+        <footer class="page-footer white z-depth-3">
+            <?php include_once($pageRepertory."footer.php"); ?>
+        </footer>
+
+    </body>
+</html>
