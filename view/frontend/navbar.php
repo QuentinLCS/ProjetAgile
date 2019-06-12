325d4049094de744c9781f94bce55a15bc962c65
Quentin <21802953@etu.unicaen.fr>
2019-06-12 12:05:11 +0200

    Déplacement de créer un utilisateur

--- view/frontend/navbar.php
+++ view/frontend/navbar.php
@@ -3,7 +3,7 @@
         <a <?php if ($page != 'home') echo 'href="/"'?> class="brand-logo center black-text">Sub<strong class="green-text ">Alligator</strong></a>
         <a href="#" data-target="mobile-demo" class="sidenav-trigger black-text "><i class="material-icons">menu</i></a>
         <ul class="right hide-on-med-and-down">
-            <li><a class="blue modal-trigger" href="#login"><strong>CREER UN INITIATEUR</strong></a></li>
+            <li><a class="blue modal-trigger" href="#login"><strong>SE CONNECTER</strong></a></li>
         </ul>
     </div>
     <div class="nav-content">
--- view/frontend/utilisateurs.php
+++ view/frontend/utilisateurs.php
@@ -1,2 +1,4 @@
+<a class="blue modal-trigger" href="#login"><strong>CREER UN INITIATEUR</strong></a>
+
 <?php
 echo "utilisateurs";
