7c00d05cc8206b8277e179b0f6193fbd58024ffa
Gérémy L'Ecuyer <chupa4000@gmail.com>
2019-06-12 16:17:44 +0200

    modif init3

--- view/frontend/initiateurs.php
+++ view/frontend/initiateurs.php
@@ -17,7 +17,7 @@ $req = <<<HEREDOC
 SELECT MEM_NUM,MEM_NOM,MEM_PRENOM,MEM_MAIL FROM PLO_MEMBRE ORDER BY MEM_NUM desc
 HEREDOC;
 
-$res = $pdoConnection->prepare($req);
+$res = $pdoConnection->query($req);
 while ($donnees = $req->fetch())
 {
 	echo $donnees['MEM_NOM'].$donnees['MEM_PRENOM'].$donnees['MEM_MAIL'];
