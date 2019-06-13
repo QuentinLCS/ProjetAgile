9ae28e54ef98eabc118b7f33ee19a9c3916ff8ff
QuentinLireux <21802144@campus.unicaen.fr>
2019-06-13 16:17:31 +0200

    Ajout

--- controller/creerSeanceController.php
+++ controller/creerSeanceController.php
@@ -28,7 +28,7 @@ try {
     $req2 = "INSERT INTO PLO_SEANCE (SEA_CODE,SEA_DATE) VALUES ('$code', '$date')";
     $base->query($req2);
 
-    header('Location: ../view/frontend/planning.php');
+    header('Location: https://dev-agile8.users.info.unicaen.fr/view/frontend/visiteur.php?page=Planning');
     exit();
 ?>
 
