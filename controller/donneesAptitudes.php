436c76c2082caf5b443da4699640bff40f062c76
Quentin Fontaine <21800691@etu.unicaen.fr>
2019-06-13 21:18:29 +0200

    donnees affichage

--- controller/donneesAptitudes.php
+++ controller/donneesAptitudes.php
@@ -2,6 +2,7 @@
 
 
 $listeDates = array();
+$tableau = array(array());
 
 function heures($idEleve){
 
@@ -17,20 +18,71 @@ function heures($idEleve){
     $i = 0;
     while($donnees = $res->fetch()){
         $listeDates[$i] = $donnees['DAT_DATE'];
+        $i++;
     }
 
 }
 
 function statutAptitude($idEleve){
 
+    global $tableau;
     global $listeDates;
+
+    include_once('../model/model.php');
+    global $base;
+
 
     heures($idEleve);
 
+
+
+    $requeteCompetences = "SELECT COM_NOM, COM_CODE FROM PLO_COMPETENCES JOIN PLO_ELEVE USING(FOR_CODE)";
+    $res = $base->query($requeteCompetences);
+
+    /*Premiere ligne (Competences)*/
+    $i=0;
+    $j=0;
+    while($donnees = $res->fetch()){
+        $tableau[0][$i] = $donnees['COM_NOM'];
+
+
+
+        /*Seconde ligne*/
+        $comCode = $donnees['COM_CODE'];
+        $requeteAptitude = "SELECT APT_NOM FROM PLO_APTITUDES WHERE COM_CODE = $comCode";
+        $resAptitude = $base->query($requeteAptitude);
+
+        while($donneesLigne2 = $resAptitude->fetch()) {
+            $tableau[1][$j] = $donneesLigne2['APT_NOM'];
+            $j++;
+        }
+        $resAptitude->closeCursor();
+        /*Fin ligne 2*/
+
+
+        $i++;
+    }
+    $res->closeCursor();
+
+
+
+    $i =2;
     foreach ($listeDates as $uneDate){
+        $tableau[$i][0] = $uneDate;
+
+
+    }
 
 
 
+    /*TEST*/
+    foreach ($tableau as $ligne){
+        foreach ($ligne as $case){
+
+            echo $case;
+
+        }
+        echo "<br>";
     }
 
 }
