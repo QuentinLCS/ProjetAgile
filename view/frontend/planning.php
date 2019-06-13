1d303d416d60a3110261a2f8298e03bf494ddabb
QuentinLireux <21802144@campus.unicaen.fr>
2019-06-13 13:19:15 +0200

    Modification du planning.php

--- view/frontend/planning.php
+++ view/frontend/planning.php
@@ -49,8 +49,9 @@ try {
                 <button class="btn waves-effect waves-light" type="submit" name="action">Submit
                     <i class="material-icons right">send</i>
                 </button>
+                <?php $reponse->closeCursor(); ?>
             </form>
-            $reponse->closeCursor();
+            
         </div>
     </div>
 </div>
