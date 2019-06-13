a9e25abcd8edaeff38e1bc413658fcf110388179
QuentinLireux <21802144@campus.unicaen.fr>
2019-06-13 15:03:09 +0200

    Ajout 2

--- controller/creerSeanceController.php
+++ controller/creerSeanceController.php
@@ -18,7 +18,7 @@ try {
 } catch (PDOException $e) {
   echo "Erreur connection : ".$e->getMessage();
 }
-
+/*
 $reponse = $bdd->query("SELECT MEM_NUM, MEM_NOM, MEM_PRENOM FROM PLO_MEMBRE");
 
 while($donnees = $reponse->fetch())
@@ -28,10 +28,9 @@ while($donnees = $reponse->fetch())
 		$membreNumMeet = $donnees['MEM_NUM'];
 	}
 }
+*/
 
-include('../view/frontend/connexionMySQL.php');
-
-$req2 = "INSERT INTO PLO_SEANCE VALUES ('$name','$membreNumMeet','$dateMeet')";
+$req2 = "INSERT INTO PLO_SEANCE(SEA_CODE, SEA_DATE) VALUES ('$name','$dateMeet')";
 $base->query($req2);
 header('Location: /index.php');
 exit();
