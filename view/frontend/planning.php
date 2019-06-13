6772b1845c0fff4702a2d16f9117a6c02ca9b894
QuentinLireux <21802144@campus.unicaen.fr>
2019-06-13 11:17:43 +0200

    Modification du planning.php

--- view/frontend/planning.php
+++ view/frontend/planning.php
@@ -9,21 +9,33 @@ echo "planning";
         <h4>Ajouter une séance</h4>
 
         <div class="row">
-            <form class="col s12" method="post" action="/controller/CreerEleve.php">
-                <div class="row">
-                    <div class="input-field col s6">
-                        <input id="PrenomEleve" type="text" class="validate" name="prenom" required>
-                        <label for="Prenom">Choisir un élève</label>
-                    </div>
-                    <div class="input-field col s6">
-                        <input id="NomEleve" type="text" class="validate" name="nom" required>
-                        <label for="Nom">entrer une date</label>
-                    </div>
-                </div>
-                <button class="btn waves-effect waves-light" type="submit" name="action">Submit
-                    <i class="material-icons right">send</i>
-                </button>
-            </form>
+            <form action="../../controller/creerSeanceController.php" method="post">
+
+            <div class="input-field">
+                <input autofocus type="text" id="name" data-length="20" name="typeSeance">
+                <label for="name"> </label>
+            </div>
+            <br>
+            <div class="input-field">
+                <select name="selectMembre">
+                    <?php
+                    include_once('../view/frontend/connexionMySQL.php');
+                    $sql = "SELECT MEM_NUM, MEM_NOM, MEM_PRENOM FROM PLO_MEMBRE";
+                    $res = mysql_query($sql) or exit(mysql_error());
+                    while($data=mysql_fetch_array($res)) {
+                       echo '<option>'$data["MEM_NOM"].$data["MEM_PRENOM"].'</option><br/>';
+                    }
+                    ?>
+                    <input type="hidden" name="numMembre" values <?php echo $data["MEM_NUM"];?>>
+                </select>
+            </div>
+            <div class="input-field">
+                <input type="date" id="start" name="dateSeance">
+            </div>
+            <br>
+            <input type="submit" value="Valider" class="btn btn-large blue">
+
+        </form>
         </div>
     </div>
 </div>
