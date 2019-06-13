d1220a055747487c6958871daa6150e329b84a2f
Gérémy L'Ecuyer <chupa4000@gmail.com>
2019-06-13 15:06:45 +0200

    fkio

--- view/frontend/initiateurs.php
+++ view/frontend/initiateurs.php
@@ -41,7 +41,7 @@ while ($donnees = $res->fetch())
     global $num;
         $num = htmlspecialchars($donnees['MEM_NUM']);
         echo "<tr> <td>".htmlspecialchars($num) . "</td><td>" .htmlspecialchars($donnees['MEM_NOM']). "</td><td>" .htmlspecialchars($donnees['MEM_PRENOM'])."</td><td>".$donnees['MEM_ROLE']."</td>";
-        if($_SESSION['num']!=$donnees['MEM_NUM'])?>
+        if($_SESSION['MEM_NUM']!=$donnees['MEM_NUM'])?>
         <td>
             <form action="../../controller/utils.php" method="post" class="usersOptions">
                 <input type="number" name="num" value="<?php echo $num ?>" style="display: none;">
