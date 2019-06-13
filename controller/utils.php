cff72ba664c82dc5dcb2a9551eb6aa2f7a6516e7
Quentin <21802953@etu.unicaen.fr>
2019-06-13 18:26:34 +0200

    Testons liste déroulante php

--- controller/utils.php
+++ controller/utils.php
@@ -17,6 +17,11 @@ elseif (isset($_POST['remUtilisateur'])) {
     supprimerDonnee("MEM_NUM = $num","PLO_MEMBRE");
 }
 
+elseif (isset($_POST['compCode'])) {
+    afficherAptitudes($_POST['compCode']);
+    exit();
+}
+
 
 function modifierRole ($NumUtilisateur, $Role) {
 
@@ -44,4 +49,50 @@ function supprimerDonnee ($condition, $table) {
 
     header('Location: ../view/frontend/visiteur.php?page=Initiateurs');
     exit();
+}
+
+function afficherAptitudes($compCode) {
+
+$dbhost = 'localhost';
+$dbuser = 'agile8';
+$dbpass = 'ahV2FeemahM6Jiex';
+$dsn = 'mysql:host=localhost;dbname=agile8_bd;charset=utf8';
+
+include_once("registerInitiateur.php");
+try {
+    $pdoConnection = new PDO($dsn, $dbuser, $dbpass);
+    $pdoConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Erreur connection : ".$e->getMessage();
+}
+
+$req = <<<HEREDOC
+SELECT * FROM PLO_APTITUDES JOIN PLO_COMPETENCES USING(COM_CODE) WHERE COM_CODE = $compCode;
+HEREDOC;
+
+$res = $pdoConnection->query($req);
+
+echo '<table class="striped centered">
+        <thead>
+            <tr>
+                <th>NUMERO</th>
+                <th>NOM</th>
+                <th>DESCRIPTION</th>  
+                <th>EDITER UNE APTITUDE...</th>         
+            </tr>
+        </thead>
+        <tbody>';
+    while ($donnees = $res->fetch()) {
+
+$num = htmlspecialchars($donnees['APT_CODE']);
+
+echo "<tr> <td>".htmlspecialchars($num) . "</td><td>" .htmlspecialchars($donnees['APT_NOM']). '</td><td>' .htmlspecialchars($donnees['APT_DESC']).'</td>'?>
+        <td>
+            <form action="utils.php" method="post" class="usersOptions">
+                <input type="text" name="aptcode" value="<?php $donnees['APT_CODE'] ?>" style="display: none;">
+                <input type="submit" name="remUtilisateur" value="X" class="grey darken-4 waves-effect waves-light small">
+            </form>
+        </td>;
+<?php 
+    }
 }
