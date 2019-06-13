bbe6e72b36666a7d9b6f489fee75de03385237ae
QuentinLireux <21802144@campus.unicaen.fr>
2019-06-13 16:00:34 +0200

    ajout 4

--- view/frontend/planning.php
+++ view/frontend/planning.php
@@ -3,17 +3,6 @@ echo "planning";
 
 //Connection Base de Donnee
 
-$dbhost = 'localhost';
-$dbuser = 'agile8';
-$dbpass = 'ahV2FeemahM6Jiex';
-$dsn = 'mysql:host=localhost;dbname=agile8_bd;charset=utf8';
-
-try {
-  $pdoConnection = new PDO($dsn, $dbuser, $dbpass);
-  $pdoConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-  echo "Erreur connection : ".$e->getMessage();
-}
 ?>
 <a class="blue waves-effect waves-light btn modal-trigger" href="#planning"><strong>Ajouter une séance</strong></a>
 
@@ -26,10 +15,11 @@ try {
             <form class="col s12" method="post" action="/controller/creerSeanceController.php">
                 <div class="row">
                     <div class="input-field col s6">
-                       <!-- <input autofocus type="text" id="name" data-length="20" name="typeSeance">
+                        <input autofocus type="text" id="name" data-length="20" name="typeSeance">
               			<label for="name"> </label>
-                        -->
-                        <input type="time" name="wakeup">
+
+                      <!--  <input type="time" name="wakeup">-->
+
 
                     </div>
                     
