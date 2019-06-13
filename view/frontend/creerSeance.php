d6fd2cd717a2cc63fc0fe048543e7dfb4690cb14
QuentinLireux <21802144@campus.unicaen.fr>
2019-06-13 11:00:52 +0200

    Modif creer seance

--- view/frontend/creerSeance.php
+++ view/frontend/creerSeance.php
@@ -17,7 +17,7 @@
                     $sql = "SELECT MEM_NUM, MEM_NOM, MEM_PRENOM FROM PLO_MEMBRE";
                     $res = mysql_query($sql) or exit(mysql_error());
                     while($data=mysql_fetch_array($res)) {
-                       echo '<option>'$data["MEM_NOM"].$data["MEM_PRENOM"].'</option><br/>';
+                       echo '<option>'.$data["MEM_NOM"].$data["MEM_PRENOM"].'</option><br/>';
                     }
                     ?>
                     <input type="hidden" name="numMembre" values <?php echo $data["MEM_NUM"];?>>
