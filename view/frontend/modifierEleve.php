e1657d0bc4682fee84e91398479ed8325921099d
LEPREVOST <21800690@etu.unicaen.fr>
2019-06-13 18:33:45 +0200

    eleve

--- view/frontend/modifierEleve.php
+++ view/frontend/modifierEleve.php
@@ -1,9 +1,25 @@
-<?php include_once("head.php");?>
+<?php include_once("head.php");
+global $base;
+
+include_once ("connexionMySQL.php");
+$num= $_SESSION['numEleve'];
+
+$req = <<<HEREDOC
+SELECT ELE_NOM, ELE_PRENOM from PLO_ELEVE WHERE ELE_NUM='$num';
+HEREDOC;
+
+$reponse=$pdoConnection->query($req);
+      
+    while($donnees = $reponse->fetch()){
+        $prenom=$donnees['ELE_PRENOM'];
+        $nom=$donnees['ELE_NOM'];
+    }
+?>
 <div id="modifierEleve">
     <div class=" center">
         <h4>Modifier élève</h4>
-
         <div class="row">
+            <h5><?php echo $prenom.' '.$nom; echo $num;?></h5>
             <form class="col s12" method="post" action="/controller/Eleve.php">
                 <div class="row">
                     <div class="input-field col s6">
