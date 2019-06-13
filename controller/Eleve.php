9899360b984a0f8a2e422e80039a80c5a36dd446
Quentin <21802953@etu.unicaen.fr>
2019-06-13 16:34:24 +0200

    Testons liste déroulante php

--- controller/Eleve.php
+++ controller/Eleve.php
@@ -22,6 +22,10 @@ elseif (isset($_POST['remEleve'])) {
     session_destroy();
 }
 
+elseif (isset($_POST['plusdinfos'])) {
+    afficher($_SESSION['numEleve']);
+}
+
 
 
 function supprimer($numEleve){
@@ -38,4 +42,50 @@ function modifier($prenom, $nom, $numEleve){
     $reqModif = "UPDATE PLO_ELEVE SET ELE_PRENOM = '$prenom', ELE_NOM = '$nom' WHERE ELE_NUM = $numEleve";
     $base->query($reqModif);
     header('Location: ../view/frontend/visiteur.php');
+}
+
+function afficher($numEleve) {
+    $dbhost = 'localhost';
+    $dbuser = 'agile8';
+    $dbpass = 'ahV2FeemahM6Jiex';
+    $dsn = 'mysql:host=localhost;dbname=agile8_bd;charset=utf8';
+
+    try {
+        $pdoConnection = new PDO($dsn, $dbuser, $dbpass);
+        $pdoConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        echo "Erreur connection : ".$e->getMessage();
+    }
+
+    $req = "SELECT * FROM PLO_APTITUDES ORDER BY APT_CODE";
+
+    $resAptitudes = $pdoConnection->query($req);
+
+    echo '<table class="striped centered">
+            <thead>
+                <tr>';
+                while ($donnees = $res->fetch()) {
+                    echo '<th>APT?</th>';
+                }
+    echo '</tr>
+            </thead>
+            <tbody>';
+
+    while ($donnees = $res->fetch())
+    {
+
+        echo "<tr> <td>".htmlspecialchars($donnees['FOR_NOM']) . "</td><td>" .htmlspecialchars($donnees['ELE_NOM']). "</td><td>" .htmlspecialchars($donnees['ELE_PRENOM'])."</td>"?>
+        <td>
+            <form action="/controller/Eleve.php" method="post" class="usersOptions">
+                <input type="number" name="numEleve" value="<?php echo $num ?>" style="display: none;">
+                <input type="submit" name="formulaireModifier" value="MODIFIER" class="red darken-2 waves-effect waves-light small">
+                <input type="submit" name="remEleve" value="X" class="grey darken-4 waves-effect waves-light small">
+            </form>
+        </td>
+        <?php
+    }
+
+    echo "</tbody> </table>";
+
+    $res->closeCursor();
 }
