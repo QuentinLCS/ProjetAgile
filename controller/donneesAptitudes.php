6a8113fcf306d3d41dc9a24373801660b773901e
Quentin Fontaine <21800691@etu.unicaen.fr>
2019-06-13 20:54:10 +0200

    donnees affichage

--- controller/donneesAptitudes.php
+++ controller/donneesAptitudes.php
@@ -1,12 +1,36 @@
 <?php
 
+
+$listeDates = array();
+
 function heures($idEleve){
+
+    global $listeDates;
 
     include_once('../model/model.php');
     global $base;
 
     $requeteDates = "SELECT DAT_DATE FROM TRAVAILLE WHERE ELE_NUM = $idEleve";
-    $_SESSION['DatesSeanceEleve'] = $base->query($requeteDates);
+    $res = $base->query($requeteDates);
 
+    /*Remplissage du tableau de date*/
+    $i = 0;
+    while($donnees = $res->fetch()){
+        $listeDates[$i] = $donnees['DAT_DATE'];
+    }
+
+}
+
+function statutAptitude($idEleve){
+
+    global $listeDates;
+
+    heures($idEleve);
+
+    foreach ($listeDates as $uneDate){
+
+
+
+    }
 
 }
