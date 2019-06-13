acd94562dc6d7a6fc5d7184893d0e636815fc659
QuentinLireux <21802144@campus.unicaen.fr>
2019-06-13 11:30:24 +0200

    Modification du planning.php

--- view/frontend/planning.php
+++ view/frontend/planning.php
@@ -15,19 +15,7 @@ echo "planning";
                         <input autofocus type="text" id="name" data-length="20" name="typeSeance">
               			<label for="name"> </label>
                     </div>
-                    <div class="input-field col s6">
-                        <select name="selectMembre">
-                            <?php
-                            include_once('connexionMySQL.php');
-                            $sql = "SELECT MEM_NUM, MEM_NOM, MEM_PRENOM FROM PLO_MEMBRE";
-                            $res = mysql_query($sql) or exit(mysql_error());
-                            while($data=mysql_fetch_array($res)) {
-                               echo '<option>'.$data["MEM_NOM"].$data["MEM_PRENOM"].'</option><br/>';
-                            }
-                            ?>
-                            <input type="hidden" name="numMembre" values <?php echo $data["MEM_NUM"];?>>
-                        </select>
-                    </div>
+                    
                     <br>
                     <div class="input-field col s6">
               			<input type="date" id="start" name="dateSeance">
