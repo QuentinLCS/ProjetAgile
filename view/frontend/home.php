c2490466b53551a4e1f553db7d33dc2fadd211a3
Gérémy L'Ecuyer <chupa4000@gmail.com>
2019-06-14 14:32:56 +0200

    eifhjeskj

--- view/frontend/home.php
+++ view/frontend/home.php
@@ -1,2 +1,11 @@
-<?php
-echo "Tableau de bord";
+<div class="container center">
+    <img src="http://www.suballigators.fr/wp-content/uploads/2017/01/logo-d%C3%A9tour%C3%A9-transparent-leger.png" alt="Logo" height="100px">
+    <h2>Bienvenue sur le panel de gestion Sub'Alligators !</h2>
+    <p class="black-text">Nous sommes un club de plongée affiliés FFESSM basé sur Caen, et vous accueillons tous les lundis et mercredis soir<br> pour un moment de détente dans les bassins nautiques de Caen.<br>
+
+        Enseignement plongée, apnée, nage …
+        Le club de plongée Sub’ Alligators, en partenariat avec les clubs de la région, organise de nombreuses plongées sur les épaves de la 2nd Guerre Mondiale, sur les sites tels que Asnelles, Arromanches, Courseulles Sur Mer …
+
+        Nous organisons également des weekends et des voyages dédiés à la plongée en Égypte, Mexique …</p>
+</div>
+
