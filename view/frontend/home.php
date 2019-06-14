35ce4a4783a525588e9fc000c84cb26b49647630
Quentin <21802953@etu.unicaen.fr>
2019-06-14 14:32:31 +0200

    Test amélioration création séance

--- view/frontend/home.php
+++ view/frontend/home.php
@@ -3,8 +3,8 @@
     <h2>Bienvenue sur le panel de gestion Sub'Alligators !</h2>
     <p class="black-text">Nous sommes un club de plongée affiliés FFESSM basé sur Caen, et vous accueillons tous les lundis et mercredis soir pour un moment de détente dans les bassins nautiques de Caen.<br><br>
 
-        Enseignement plongée, apnée, nage …<br>
-        Le club de plongée Sub’ Alligators, en partenariat avec les clubs de la région, organise de nombreuses plongées sur les épaves de la 2nd Guerre Mondiale, sur les sites tels que Asnelles, Arromanches, Courseulles Sur Mer …<br>
+        Enseignement plongée, apnée, nage …<br><br>
+        Le club de plongée Sub’ Alligators, en partenariat avec les clubs de la région, organise de nombreuses plongées sur les épaves de la 2nd Guerre Mondiale, sur les sites tels que Asnelles, Arromanches, Courseulles Sur Mer …<br><br>
 
         Nous organisons également des weekends et des voyages dédiés à la plongée en Égypte, Mexique …</p>
 </div>
