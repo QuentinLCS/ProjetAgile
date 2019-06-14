886301eed2dc6d9c3047e215fcadda7f8230af61
Quentin <21802953@etu.unicaen.fr>
2019-06-14 10:15:48 +0200

    Gestion de séances et comptes rendu

--- controller/creerSeance.php
+++ controller/creerSeance.php
@@ -3,28 +3,29 @@ global $base;
 
 include_once('../model/model.php');
 
-$eleveNum = $_POST['eleve'];
-$aptitude = $_POST['aptitude'];
+$niveauFormation = $_POST['niveauFormation'];
 $date = $_POST['date'];
 $heure = $_POST['heure'];
-$commentaire = $_POST['commentaire'];
 
-$req = 'SELECT MAX(MEM_NUM) FROM PLO_MEMBRE';
-$res = $base->query($req);
+include_once("../view/frontend/connexionMySQL.php");
 
-$dateheure = $date." ".$heure;
+$req0 = <<<HEREDOC
+SELECT * FROM PLO_ELEVE WHERE FOR_CODE = '$niveauFormation' ORDER BY ELE_NOM asc;
+HEREDOC;
 
-$req2 = "INSERT INTO TRAVAILLE(ELE_NUM, APT_CODE, DAT_DATE, EVA_COMMENTAIRE) VALUES ('$eleveNum', '$aptitude', STR_TO_DATE('$dateheure', '%Y-%m-%d %H:%i'), '$commentaire')";
-$base->query($req2);
+$res0 = $pdoConnection->query($req0);
 
+while ($donnees0 = $res0->fetch()) {
 
-if (isset($_POST['estValide'])) {
-    $estValide = $_POST['estValide'];
+    $eleveNum = $donnees0['ELE_NUM'];
 
-    $req3 = 'UPDATE ';
+    echo $eleveNum." ; ".$dateheure." ; ".$niveauFormation;
 
+    $dateheure = $date . " " . $heure;
+
+    $req2 = "INSERT INTO TRAVAILLE(ELE_NUM, APT_CODE, DAT_DATE) VALUES ('$eleveNum', 'PAS ENCORE PASSEE', STR_TO_DATE('$dateheure', '%Y-%m-%d %H:%i'))";
+    $base->query($req2);
 }
-
-header('Location: ../view/frontend/visiteur.php?page=planning');
-exit();
+//header('Location: ../view/frontend/visiteur.php?page=planning');
+//exit();
 ?>
